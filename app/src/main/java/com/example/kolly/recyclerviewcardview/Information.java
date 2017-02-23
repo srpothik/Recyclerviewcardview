438c07188d03ec515c2f35d354e46ede12b57c9e
srpothik <srpothik619@gmail.com>
2017-02-23 13:39:16 +0600

    Initial commit

---
+++ app/src/main/java/com/example/kolly/recyclerviewcardview/Information.java
@@ -0,0 +1,12 @@
+package com.example.kolly.recyclerviewcardview;
+
+/**
+ * Created by kolly on 2/22/2017.
+ */
+
+
+public class Information {
+
+    public int imageId;
+    public String title;
+}
